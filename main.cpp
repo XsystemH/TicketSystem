375134f4c39247e107145e669e893932bf79d0d9
Evangelion 4 <1319971303@qq.com>
2024-05-12 06:20:33 +0800

    No Hash

--- main.cpp
+++ main.cpp
@@ -6,7 +6,59 @@
 
 #define K 277
 
-BPT<unsigned long long, int> s;
+class MyString {
+public:
+  char a[64];
+  int len;
+
+public:
+  MyString() {
+    len = 0;
+  }
+  MyString(std::string s) {
+    for (char c : s) {
+      a[len++] = c;
+    }
+  }
+  ~MyString() = default;
+
+  bool operator<(const MyString &other) const {
+    if (len != other.len) return len < other.len;
+    for (int i = 0; i < len; ++i) {
+      if (a[i] != other.a[i]) return a[i] < other.a[i];
+    }
+    return false;
+  }
+
+  bool operator==(const MyString &other) const {
+    if (len != other.len) return false;
+    for (int i = 0; i < len; ++i) {
+      if (a[i] != other.a[i]) return false;
+    }
+    return true;
+  }
+
+  bool operator>=(const MyString &other) const {
+    return !(*this < other);
+  }
+
+  bool operator<=(const MyString &other) const {
+    return (*this < other) || (*this == other);
+  }
+
+  bool operator>(const MyString &other) const {
+    return !(*this <= other);
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const MyString& s) {
+    for(int i = 0; i < s.len; ++i) {
+      os << s.a[i];
+    }
+    return os;
+  }
+};
+
+BPT<MyString, int> s;
 
 unsigned long long hash(std::string &s) {
   unsigned long long ans = 0;
@@ -28,15 +80,15 @@ int main() {
     std::cin >> opt;
     if (opt == "insert") {
       std::cin >> index >> value;
-      s.insert(hash(index), value);
+      s.insert(MyString(index), value);
     }
     else if (opt == "delete") {
       std::cin >> index >> value;
-      s.erase(hash(index), value);
+      s.erase(MyString(index), value);
     }
     else if (opt == "find") {
       std::cin >> index;
-      vector<int> ans = s.find(hash(index));
+      vector<int> ans = s.find(MyString(index));
       if (!ans.empty()) for (int a : ans) {
         std::cout << a << " ";
       }
