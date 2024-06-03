5a44e1b316e45765b0a207d7a5aa8582661e7b7a
Evangelion 4 <1319971303@qq.com>
2024-06-03 12:44:37 +0800

    std::ios::sync_with_stdio(0),std::cin.tie(0),std::cout.tie(0);

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,7 @@
 int main() {
 //  freopen("in", "r", stdin);
 //  freopen("out", "w", stdout);
+  std::ios::sync_with_stdio(0),std::cin.tie(0),std::cout.tie(0);
   CMD cmd;
   while (true) {
     cmd = get_cmd();
